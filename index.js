04fbec00727a923b09a7c050f1fbb694ab530d06
daru darmakusuma <darudarmakusuma@gmail.com>
2021-02-24 22:53:03 +0700

    edit vertices and size (not rendered):

--- index.js
+++ index.js
@@ -19,9 +19,15 @@ let setColor = vec4(0.0, 0.0, 0.0, 1.0);
 let drawingHistory = []; // used to keep track of the drawing we have so far
 let drawingPoints = []; // used when drawing a new set of points
 let pointHistory = [];
+let shapes = [];
 let colorHistory = [];
+let mousePos = {
+  x: 0,
+  y: 0
+}
 
 let drawMode = true;
+let isMove = false;
 let statusdraw = "Nothing"; 
 let newLine = false;
 
@@ -32,37 +38,69 @@ function main() {
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = WebGLUtils.setupWebGL(canvas, undefined);
+  gl = canvas.getContext('webgl', {preserveDrawingBuffer: true});
   if (!gl) {
       console.log('Failed to get the rendering context for WebGL');
       return;
   }
 
   // Initialize shaders
-  program = initShaders(gl, "vshader", "fshader");
-  gl.useProgram(program);
+  var vertSrc = `precision mediump float;
+    
+    attribute vec2 vertPos;
+    uniform vec2 screenRes;
+    uniform mat3 transToOrigin;
+    uniform mat3 scaleMat;
+    uniform mat3 transBack;
 
+    void main() {
+        vec3 transResult = transBack * scaleMat * transToOrigin * vec3(vertPos, 1.0);
+        vec2 newPos = 2.0*transResult.xy/screenRes;
+        gl_Position = vec4(newPos, 0.0, 1.0);
+    }`;
+
+  var fragSrc = `precision mediump float;
+
+  uniform vec4 u_fragColor;
+  void main() {
+      gl_FragColor = u_fragColor;
+  }`;
+
+  var vertShader = gl.createShader(gl.VERTEX_SHADER);
+  var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertShader, vertSrc);
+  gl.shaderSource(fragShader, fragSrc);
+
+  gl.compileShader(vertShader);
+  if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
+      console.log("failed to compile vertex shader, ", gl.getShaderInfoLog(vertShader));
+      return;
+  }
+
+  gl.compileShader(fragShader);
+  if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+      console.log("failed to compile fragment shader, ", gl.getShaderInfoLog(fragShader));
+      return;
+  }
+
+  program = gl.createProgram();
+  gl.attachShader(program, vertShader);
+  gl.attachShader(program, fragShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.log("failed to link program : ", gl.getProgramInfoLog(program));
+      return;
+  }
+  
   // Set up the canvas
   canvasWidth = canvas.width;
   canvasHeight = canvas.height;
 
-  // Buffer for our points
-  pBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-
-  // For our vertex positions
-  vPosition = gl.getAttribLocation(program, "vPosition");
-  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPosition);
-
-  // Buffer for our colors
-  cBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-
-  // For our vertex colors
-  vColor = gl.getAttribLocation(program, "vColor");
-  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vColor);
+  // Set canvas event listener
+  canvas.addEventListener('mousemove', canvasOnMouseMove);
+  canvas.addEventListener('mousedown', canvasOnMouseDown);
+  canvas.addEventListener('mouseup', canvasOnMouseUp);
 
   canvas.onclick = function (e) {
       if (drawMode === true) {
@@ -86,6 +124,34 @@ function main() {
                 newLine = true;
             }
             // Push these points to the drawing history
+            shapes.push(
+              {
+                  type: 'line',
+                  vertices: [
+                      pos.x, pos.y,
+                      pos.x + gl.canvas.width/8, pos.y
+                  ],
+                  scaleMat: [
+                      1.0, 0, 0,
+                      0, 1.0, 0,
+                      0, 0, 1.0
+                  ],
+                  transToOrigin: [
+                      1.0, 0, 0,
+                      0, 1.0, 0,
+                      0, 0, 1.0
+                  ],
+                  transBack: [
+                      1.0, 0, 0,
+                      0, 1.0, 0,
+                      0, 0, 1.0
+                  ],
+                  mode: gl.LINES,
+                  color: setColor,
+                  type: 'line',
+                  id: 0,
+              }
+            );
             drawingPoints.push(vec4(parseFloat(pos.x), parseFloat(pos.y), 0.0, 1.0));
             drawingHistory.push(drawingPoints);     
             colorHistory.push(setColor);  
@@ -108,51 +174,42 @@ function main() {
           if (statusdraw === "Square") {
             console.log("Drawing Square");
             pointHistory.push(pos);
-            if (pointHistory.length === 2){
-                newLine = true;
-                length = pointHistory[1].x - pointHistory[0].x ; 
-                drawingPoints.push(vec4(parseFloat(pointHistory[0].x), parseFloat(pointHistory[0].y), 0.0, 1.0));
-                drawingPoints.push(vec4(parseFloat(pointHistory[0].x + length), parseFloat(pointHistory[0].y ), 0.0, 1.0));
-                if (pointHistory[1].y > pointHistory[0].y ){
-                    if (length > 0 ) {
-                        drawingPoints.push(vec4(parseFloat(pointHistory[0].x + length), parseFloat(pointHistory[0].y + length), 0.0, 1.0));//
-                        drawingPoints.push(vec4(parseFloat(pointHistory[0].x ), parseFloat(pointHistory[0].y + length), 0.0, 1.0));//
-                    } else {
-                        drawingPoints.push(vec4(parseFloat(pointHistory[0].x + length), parseFloat(pointHistory[0].y - length), 0.0, 1.0));
-                        drawingPoints.push(vec4(parseFloat(pointHistory[0].x ), parseFloat(pointHistory[0].y - length), 0.0, 1.0));
-                    }
-                } else {
-                    if (length > 0 ){
-                    drawingPoints.push(vec4(parseFloat(pointHistory[0].x + length), parseFloat(pointHistory[0].y - length), 0.0, 1.0));
-                    drawingPoints.push(vec4(parseFloat(pointHistory[0].x ), parseFloat(pointHistory[0].y - length), 0.0, 1.0));
-                    }
-                    else {
-                    drawingPoints.push(vec4(parseFloat(pointHistory[0].x + length), parseFloat(pointHistory[0].y +   length), 0.0, 1.0));
-                    drawingPoints.push(vec4(parseFloat(pointHistory[0].x ), parseFloat(pointHistory[0].y + length), 0.0, 1.0));        
-                    }
-                }
-
-                drawingPoints.push(vec4(parseFloat(pointHistory[0].x), parseFloat(pointHistory[0].y), 0.0, 1.0));
-                drawingHistory.push(drawingPoints);
-                colorHistory.push(setColor);
-                pointHistory = [];
-            }
-            // p1 = {x1,y1} p3 = {x2,y2} p2 = {x1,y2} p4 = {x2,y2}
-                    
+            shapes.push(
+              {
+                type: 'square',
+                midPoint: [pos.x + gl.canvas.width/16, pos.y - gl.canvas.height/16],
+                vertices: [
+                    pos.x, pos.y,
+                    pos.x + gl.canvas.width/8, pos.y,
+                    pos.x + gl.canvas.width/8, pos.y - gl.canvas.width/8,
+                    pos.x, pos.y,
+                    pos.x + gl.canvas.width/8, pos.y - gl.canvas.width/8,
+                    pos.x, pos.y - gl.canvas.width/8
+                ],
+                scaleMat: [
+                    1.0, 0, 0,
+                    0, 1.0, 0,
+                    0, 0, 1.0
+                ],
+                transToOrigin: [
+                    1.0, 0, 0,
+                    0, 1.0, 0,
+                    0, 0, 1.0
+                ],
+                transBack: [
+                    1.0, 0, 0,
+                    0, 1.0, 0,
+                    0, 0, 1.0
+                ],
+                mode: gl.TRIANGLES,
+                color: setColor,
+                id: 1
+              });
           }
-
-
-          if (newLine) {
-              console.log("done");
-              console.log(drawingHistory);
-              drawingPoints = [];
-              newLine = false;
-          }
-          for (let d in drawingHistory) {
-              drawLine(drawingHistory[d],colorHistory[d]);
-          }
+          // p1 = {x1,y1} p3 = {x2,y2} p2 = {x1,y2} p4 = {x2,y2}                
       }
   };
+  requestAnimationFrame(render);
 }
 function drawSquare(){
     document.getElementById("mode").innerHTML = "Draw Mode : Square";
@@ -183,7 +240,172 @@ function drawConditional(){
     drawMode = true;
 }
 
+function canvasOnMouseMove(event) {
+  let rect = canvas.getBoundingClientRect();
+  let xMax = gl.canvas.width / 2;
+  let yMax = gl.canvas.height / 2;
 
+  mousePos.x = event.clientX - rect.x - xMax;
+  mousePos.y = rect.y - event.clientY + yMax;
+}
+
+function canvasOnMouseUp(event) {
+  move = false;
+}
+
+function canvasOnMouseDown(event) {
+  activeObject = null;
+  isMove = false;
+  chosenIdx = -1;
+  document.getElementById("scale-box").style.display = 'none';
+  for (shape of shapes) {
+      oldVertices = [];
+      oldMidPoint = [];
+      for (let i = 0; i < shape.vertices.length; i += 2) {
+          let mat = matrixMultiplication(
+              matrixTranspose(shape.transBack), 
+              matrixMultiplication(matrixTranspose(shape.scaleMat), matrixTranspose(shape.transToOrigin))
+          );
+          let transformedVertice = transform(mat, [shape.vertices[i], shape.vertices[i + 1], 1.0])
+          oldVertices.push(shape.vertices[i]);
+          oldVertices.push(shape.vertices[i + 1]);
+          if (Math.abs(transformedVertice[0] - mousePos.x) < 5 && Math.abs(transformedVertice[1] - mousePos.y) < 5) {
+              isMove = true;
+              activeObject = shape;
+              startX = mousePos.x;
+              startY = mousePos.y;
+              chosenIdx = i;
+              if (shape.type === 'square') {
+                  oldMidPoint.push(shape.midPoint[0]);
+                  oldMidPoint.push(shape.midPoint[1]);
+              }
+          }
+      }
+      if (isMove)
+          break;
+  }
+
+  if (isMove) {
+      if (activeObject.type === 'square') {
+          document.getElementById("scale-box").style.display = 'inline';
+      }
+  }
+}
+
+function moveObject() {
+  if (isMove) {
+    let xTrans = mousePos.x - startX;
+    let yTrans = mousePos.y - startY;
+    if (activeObject.type === 'line') {
+        activeObject.vertices[chosenIdx] = oldVertices[chosenIdx] + xTrans;
+        activeObject.vertices[chosenIdx + 1] = oldVertices[chosenIdx + 1] + yTrans;
+    } else {
+        for (let i = 0; i < activeObject.vertices.length; i += 2) {
+            activeObject.vertices[i] = oldVertices[i] + xTrans;
+            activeObject.vertices[i + 1] = oldVertices[i + 1] + yTrans;
+        }
+
+        if (activeObject.type === 'square') {
+            activeObject.midPoint[0] = oldMidPoint[0] + xTrans;
+            activeObject.midPoint[1] = oldMidPoint[1] + yTrans;
+            let translateToOrigin = [
+                1.0, 0.0, 0.0,
+                0.0, 1.0, 0.0,
+                -activeObject.midPoint[0], -activeObject.midPoint[1], 1.0
+            ];
+            let translateBack = [
+                1.0, 0.0, 0.0,
+                0.0, 1.0, 0.0,
+                activeObject.midPoint[0], activeObject.midPoint[1], 1.0
+            ];
+            activeObject.transToOrigin = translateToOrigin;
+            activeObject.transBack = translateBack;
+        }
+    }
+  }
+}
+
+function drawObject(shape, program) {
+  let buf = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.vertices), gl.STATIC_DRAW);
+
+  gl.useProgram(program);
+  let pos = gl.getAttribLocation(program, 'vertPos');
+  gl.vertexAttribPointer(
+      pos,
+      2,
+      gl.FLOAT,
+      gl.FALSE,
+      2 * Float32Array.BYTES_PER_ELEMENT,
+      0
+  );
+  
+  var uniformCol = gl.getUniformLocation(program, 'u_fragColor')
+  gl.uniform4fv(uniformCol, shape.color);
+  gl.enableVertexAttribArray(pos);
+
+  let matLocation = gl.getUniformLocation(program, 'transToOrigin');
+  gl.uniformMatrix3fv(matLocation, false, new Float32Array(shape.transToOrigin));
+  matLocation = gl.getUniformLocation(program, 'scaleMat');
+  gl.uniformMatrix3fv(matLocation, false, new Float32Array(shape.scaleMat));
+  matLocation = gl.getUniformLocation(program, 'transBack');
+  gl.uniformMatrix3fv(matLocation, false, new Float32Array(shape.transBack));
+
+  gl.drawArrays(shape.mode, 0, shape.vertices.length/2);
+}
+
+function matrixTranspose(mat) {
+  let copyMat = [];
+  for (elmt of mat) {
+      copyMat.push(elmt);
+  }
+
+  for (let i = 0; i < 3; i++) {
+      for (let j = i; j < 3; j++) {
+          let temp = copyMat[3*i + j];
+          copyMat[3*i + j] = copyMat[3*j + i];
+          copyMat[3*j + i] = temp;
+      }
+  }
+
+  return copyMat;
+}
+
+function matrixMultiplication(matA, matB) {
+
+  if (matA.length != matB.length) {
+      return;
+  }
+
+  let outputMat = [];
+  let matLength = 3;
+  for (let i = 0; i < matLength; i++) {
+      for (let j = 0; j < matLength; j++) {
+          let sum = 0;
+          for (let k = 0; k < matLength; k++) {
+              sum += matA[i*matLength + k] * matB[k*matLength + j];
+          }
+          outputMat.push(sum);
+      }
+  }
+
+  return outputMat;
+}
+
+function transform(mat, vertice) {
+  let tempVertice = [];
+  let n = vertice.length;
+  for (let i = 0; i < n; i++) {
+      let sum = 0.0;
+      for (let j = 0; j < n; j++) {
+          sum += (mat[i*n + j] * vertice[j]);
+      }
+      tempVertice.push(sum);
+  }
+
+  return tempVertice;
+}
 
 /**
 * Get the coordinates of the mouse and transform them into suitable coordinates for WebGL
@@ -278,5 +500,16 @@ function resetCanvas() {
   // Resize canvas and viewport
   adjustCanvasViewport(640, 480);
 }
+
+function render() {
+  moveObject();
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.useProgram(program);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+  for (shape of shapes) {
+      drawObject(shape, program);
+  }
+  requestAnimationFrame(render);
+};
 
  
