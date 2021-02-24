a4a98b8c5024e14412837338cf73ec09bd39e24f
DanielR <13518075@std.stei.itb.ac.id>
2021-02-24 16:49:35 +0700

    Add with object (but have error)

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ var gl;
 var maxNumVertices  = 200;
 var index = 0;
 
+var vertexArray = [];
+
+const notSelected = -1;
+
 var cindex = 0;
+
+var triangleData = [
+    0.1, 0.1,
+    1.0, 0.0,
+    0.0, 1.0
+]
 
 var colors = [
 
@@ -33,23 +43,47 @@ window.onload = function init() {
     var m = document.getElementById("mymenu");
 
     m.addEventListener("click", function() {
-       cindex = m.selectedIndex;
-        });
+        if (cindex == m.selectedIndex) {
+            var elementOption = m.options;
+            elementOption[cindex].selected = false;
+            cindex = notSelected;
+        } else {
+            cindex = m.selectedIndex;
+            // console.log(cindex);
+        }
+    });
 
     var a = document.getElementById("Button1")
     a.addEventListener("click", function(){
-    numPolygons++;
-    numIndices[numPolygons] = 0;
-    start[numPolygons] = index;
-    render();
+        numPolygons++;
+        numIndices[numPolygons] = 0;
+        start[numPolygons] = index;
+
+        console.log(cindex);
+        console.log(vertexArray);
+        console.log(vec4(colors[cindex]));
+
+        glObject.setVertexArray(vertexArray);
+        glObject.setColorArray(vec4(colors[cindex]));
+        glObject.bind();
+        glObject.draw();
+        vertexArray = [];
+        // render();
     });
 
     canvas.addEventListener("mousedown", function(event){
-        t  = vec2(2*event.clientX/canvas.width-1,
-           2*(canvas.height-event.clientY)/canvas.height-1);
-        gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-        gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+        t  = vec2(2*(event.clientX-8)/canvas.width-1,
+           2*(canvas.height-event.clientY+8)/canvas.height-1);
 
+        
+        // gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+        // gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
+        // console.log(flatten(t));
+
+        vertexArray.push(t);
+        // console.log(vertexArray);
+
+        /*
         t = vec4(colors[cindex]);
 
         gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId );
@@ -57,6 +91,7 @@ window.onload = function init() {
 
         numIndices[numPolygons]++;
         index++;
+        */
     } );
 
 
@@ -71,19 +106,32 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW );
+    // gl.bufferData( gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(triangleData), gl.STATIC_DRAW );
     var vPos = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPos, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPos );
 
     var cBufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
+    // gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors[cindex]), gl.STATIC_DRAW );
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
+    
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 3);
+
+    // const glObject = new GLObject(0, program, gl);
+    // glObject.setVertexArray(vertexArray);
+    // glObject.setColorArray(vec4(colors[cindex]));
+    // glObject.bind();
+    // glObject.draw();
+
+    console.log("hoho");
 }
 
 function render() {
---
+++ renderer.js
@@ -0,0 +1,36 @@
+class Renderer {
+    objectList;
+    count;
+
+
+    constructor() {
+        this.objectList = new Array();
+        this.count = 0;
+    }
+
+
+    addObject(obj) {
+        this.objectList.push(obj)
+        this.count++
+    }
+
+
+    removeObject(id) {
+        const idx = this.objectList.findIndex(obj => obj.id === id)
+        this.objectList.splice(idx, 1)
+        this.count--
+    }
+
+
+    render() {
+        for (const obj of this.objectList) {
+            obj.draw()
+        }
+    }
+
+    renderTex(selectProgram) {
+        for (const obj of this.objectList) {
+            obj.drawSelect(selectProgram)
+        }
+    }
+}
